03ed7e3fe8e9bc8e2a48228aa3efc971a5b439db
Yazil Ansari <yazilansari@gmail.com>
2025-02-06 14:46:41 +0400

    Changed API URL

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
           },
           {
             protocol: 'https',
-            hostname: 'admin.ahmedalmaghribi.om',
+            hostname: 'adminom.ahmedalmaghribi.com',
           },
           {
             protocol: 'http',
