e7dd993488978ecdffcc8286ce042dca64266d22
Saad Edroos <saadedroossaad@gmail.com>
2025-02-15 11:47:51 +0400

    Added AHL banner

--- data/heroslides.js
+++ data/heroslides.js
@@ -266,6 +266,16 @@ export const slidesData3 = [
     href: "shop/perfumes/occidental-fragrance/zumar",
     color: "text-white"
   },
+  {
+    id: 4,
+    imageSrc: "/assets/images/banner/AHL-Banner.jpg",
+    season: "AHL",
+    objectPosition: "70% center",
+    title: "The Essence",
+    subtitle: "of Belonging",
+    href: "shop/perfumes/oriental-fragrance/ahl",
+    color: "text-black"
+  },
   
 ];
 export const slidesData33 = [
@@ -297,6 +307,16 @@ export const slidesData33 = [
     title: "Feel Luxurious",
     subtitle: "with",
     href: "shop/perfumes/occidental-fragrance/la-rosee",
+    color: "text-black"
+  },
+  {
+    id: 4,
+    imageSrc: "/assets/images/banner/AHL-Banner.jpg",
+    season: "AHL",
+    objectPosition: "70% center",
+    title: "The Essence",
+    subtitle: "of Belonging",
+    href: "shop/perfumes/oriental-fragrance/ahl",
     color: "text-black"
   },
 
