7edf76c0eb8b776e30af60d1937697b6c1e6f7c0
“Arshad <arshad@ahmedalmaghribi.com>
2025-02-15 17:51:57 +0400

    added ahl

--- app/[locale]/(shopSingle)/shop/[...product]/page.jsx
+++ app/[locale]/(shopSingle)/shop/[...product]/page.jsx
@@ -11,7 +11,7 @@ export const metadata = {
   title: "Perfumes | Buy Best Perfumes Online | Ahmed Perfume",
   description: "Buy Best Perfumes Online Ahmed Perfume",
   icons: {
-    icon: "https://www.ahmedalmaghribi.com/wp-content/uploads/2021/08/Ahmed-Logo-e1631552829722-100x100.png",
+    icon: "/assets/images/ahmed-favicon.png",
   },
 };
 
