61f1e190da593a085a96fc4e736133d77768dcdc
Saad Edroos <saadedroossaad@gmail.com>
2025-02-25 14:22:10 +0400

    Remove banner

--- data/heroslides.js
+++ data/heroslides.js
@@ -236,16 +236,16 @@ export const slidesData2 = [
   },
 ];
 export const slidesData3 = [
-  {
-    id: 1,
-    imageSrc: "/assets/images/banner/oman-sale.jpg",
-    season: "",
-    objectPosition: "50% center",
-    title: "",
-    subtitle: "",
-    href: "shop/",
-    color: "text-black"
-  },
+  // {
+  //   id: 1,
+  //   imageSrc: "/assets/images/banner/oman-sale.jpg",
+  //   season: "",
+  //   objectPosition: "50% center",
+  //   title: "",
+  //   subtitle: "",
+  //   href: "shop/",
+  //   color: "text-black"
+  // },
   {
     id: 2,
     imageSrc: "/assets/images/la-rose.jpg",
@@ -279,16 +279,16 @@ export const slidesData3 = [
   
 ];
 export const slidesData33 = [
-  {
-    id: 1,
-    imageSrc: "/assets/images/banner/oman-mobile.jpg",
-    season: "",
-    objectPosition: "50% center",
-    title: "",
-    subtitle: "",
-    href: "shop/",
-    color: "text-black"
-  },
+  // {
+  //   id: 1,
+  //   imageSrc: "/assets/images/banner/oman-mobile.jpg",
+  //   season: "",
+  //   objectPosition: "50% center",
+  //   title: "",
+  //   subtitle: "",
+  //   href: "shop/",
+  //   color: "text-black"
+  // },
   {
     id: 2,
     imageSrc: "/assets/images/zumar-bnr.jpg",
