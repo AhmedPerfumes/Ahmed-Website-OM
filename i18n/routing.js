5d4f59d905739a0019516f657f715aae7fe1aad6
Saad Edroos <saadedroossaad@gmail.com>
2025-10-27 14:57:54 +0400

    change routing

--- i18n/routing.js
+++ i18n/routing.js
@@ -2,14 +2,10 @@ import { defineRouting } from 'next-intl/routing';
 import { createNavigation } from 'next-intl/navigation';
 
 export const routing = defineRouting({
-  // A list of all locales that are supported
   locales: ['en', 'ar'],
-
-  // Used when no locale matches
-  defaultLocale: 'ar'
+  defaultLocale: 'ar',
+  localeDetection: false, // optional: always Arabic by default
 });
 
-// Lightweight wrappers around Next.js' navigation APIs
-// that will consider the routing configuration
 export const { Link, redirect, usePathname, useRouter } =
   createNavigation(routing);
