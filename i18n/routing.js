154a19748728355a4759309b98f06d1d062fe835
Saad Edroos <saadedroossaad@gmail.com>
2025-10-29 16:47:47 +0400

    change

--- i18n/routing.js
+++ i18n/routing.js
@@ -9,6 +9,8 @@ export const routing = defineRouting({
   defaultLocale: 'ar'
 });
 
+
+
 // Lightweight wrappers around Next.js' navigation APIs
 // that will consider the routing configuration
 export const { Link, redirect, usePathname, useRouter } =
