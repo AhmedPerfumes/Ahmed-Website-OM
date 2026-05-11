503211cdf253c1e1de8cbe99b925544130b396b6
Saad Edroos <saadedroossaad@gmail.com>
2026-05-11 09:19:33 +0400

    Added track order # also

--- components/shopCartandCheckout/OrderTrack.jsx
+++ components/shopCartandCheckout/OrderTrack.jsx
@@ -110,8 +110,17 @@ export default function OrderTrack() {
               className="form-control"
               id="order_tracking_id"
               placeholder="Order ID *"
+              value={orderNumber}
               name="order_number"
               required
+              onChange={(e) => {
+                const input = e.target.value;
+                if (!input.startsWith("#")) {
+                  setOrderNumber("#" + input.replaceAll("#", ""));
+                } else {
+                  setOrderNumber(input);
+                }
+              }}
             />
             <label htmlFor="order_tracking_id">Order ID *</label>
           </div>
