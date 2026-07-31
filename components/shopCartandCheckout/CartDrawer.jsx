4bf4616aab766f0516f0b825fcca5eb8e448af8e
saood ghare <saoodghare0809@gmail.com>
2026-07-31 17:02:57 +0400

    bogo cartdrawer removed

--- components/shopCartandCheckout/CartDrawer.jsx
+++ components/shopCartandCheckout/CartDrawer.jsx
@@ -263,7 +263,7 @@ export default function CartDrawer() {
             )}
           </div> */}
 
-          {
+          {/* {
             (() => {
               // Only count non-excluded products
               const regularProducts = cartProducts.filter((item) => item.category_name && !['gift sets', 'collections'].includes(item.category_name.toLowerCase()));
@@ -357,7 +357,7 @@ export default function CartDrawer() {
                 </div>
               );
             })()
-          }
+          } */}
 
           <hr className="cart-drawer-divider" />
 
