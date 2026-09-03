7392a40a0c9dbb36df588149b3a2e345455772cc
Edroossaad <saadedroossaad@gmail.com>
2026-09-03 09:54:15 +0400

    Added seo on product page

--- app/[locale]/(shopSingle)/shop/[...product]/page.jsx
+++ app/[locale]/(shopSingle)/shop/[...product]/page.jsx
@@ -8,6 +8,7 @@ import { allProducts } from "@/data/products";
 import MobileFooter2 from "@/components/footers/MobileFooter2";
 import Head from "next/head";
 import { headers } from 'next/headers';
+import CollapsibleDescription from "@/components/shoplist/CollapsibleDescription";
 
 // export const metadata = {
 //   title: "Perfumes | Buy Best Perfumes Online | Ahmed Perfume",
@@ -225,6 +226,8 @@ const ProductDetailsPage16 = async ({ params }) => {
         <main className="page-wrapper">
           <SingleProduct11 category={categoryName} subcategory={subCategoryName} product={data} />
           <RelatedSlider relatedProds={data.related_prods} />
+           <CollapsibleDescription description={data.seo_content} title="About this Product" />
+          
         </main>
         <section className="d-none d-lg-block" style={{ height: "100%" }}>
           <Footer14 />
